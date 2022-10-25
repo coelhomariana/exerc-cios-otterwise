88e78ba70829097dcab29f2d056c9ee7589f021a
Mariana Coelho <coelhomariana@outlook.com>
2022-10-25 18:49:45 -0300

    ex10

---
+++ ex10.js
@@ -0,0 +1,13 @@
+const compareNumbers = (num1, num2) => {
+    if (num1 > num2) {
+        console.log(num1, "é maior que", num2)
+    } else if (num1 === num2) {
+        console.log(num1, "é igual a", num2)
+    } else {
+        console.log(num1, "é menor que", num2)
+    }
+}
+
+compareNumbers(10,5)
+compareNumbers(5,5)
+compareNumbers(5,20)
